e4996dc747baf582b0dab665cedf03db43bba912
Anthony Chen <anthonychen33@gmail.com>
2024-03-03 03:43:00 -0500

    Worked on signup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -7,10 +7,16 @@ import Bar from './Bar';
 
 
 const SignUp = () => {
+    const URL = 'http://localhost:3001';
+
     const [email, setEmail] = useState("");
 
+    const [errors, setErrors] = useState("");
+
     const [signUpData, setSignUpData] = useState({
-        signUpEmail: ''
+        signUpUser: '',
+        signUpPassword: '',
+        signUpPasswordConfirm: '',
     });
 
     const handleChange = (evt) => {
@@ -21,26 +27,93 @@ const SignUp = () => {
         console.log(signUpData);
     };
 
+    const signUpClicked = async () => {
+        
+        const result = await fetch(`${URL}/`, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(signUpData),
+        })
+        .then((res) => {
+            console.log(res);
+
+            if(res.status === 403) {
+                const jsonData = res.json();
+
+                return jsonData.then((data) => {
+                    const signUpError = data.errors[0].path;
+                    console.log(signUpError);
+                    if(signUpError === 'signUpUser') {
+                        throw Error('Username too short, at least 5 characters');
+                    }
+                })
+            } else if (res.status === 404) {
+                const jsonData = res.json();
+
+                return jsonData.then((data) => {
+                    throw Error(data.message);
+                })
+            }
+        })
+        .catch((err) => {
+            console.log(`Error from signUp.js ${err.message}`);
+            setErrors(err.message);
+        })
+    }
+
 
 
     return (
         <>
           <Bar />
-          
+
+          {errors.length > 0 ? <div>{errors}</div> : <></>}
+
+          <Container className="container-box">
           <InputGroup size="sm">
-            <InputGroup.Text id="emailLogo">@</InputGroup.Text>
+            <InputGroup.Text id="userLogo">U</InputGroup.Text>
             <Form.Control 
-                id="emailInput"
-                name="signUpEmail"
-                placeholder="Email"
-                aria-label="Email"
+                name="signUpUser"
+                placeholder="Username"
+                aria-label="Username"
                 aria-describedby="basic-addon1"
                 onChange={handleChange}
             />
 
+            
+
           </InputGroup>
           
-          <Button variant="secondary" size="sm">Sign Up</Button> 
+          
+          <InputGroup size="sm">
+            <InputGroup.Text id="passwordLogo">P</InputGroup.Text>    
+            <Form.Control
+                name="signUpPassword"
+                type="password"
+                placeholder="Password"
+                aria-label="Password"
+                aria-describedby="basic-addon1"
+                onChange={handleChange}
+            />
+          </InputGroup>
+
+          <InputGroup size="sm">
+            <InputGroup.Text id="confirmPasswordLogo">CP</InputGroup.Text>    
+            <Form.Control
+                name="signUpPasswordConfirm"
+                type="password"
+                placeholder="Confirm Password"
+                aria-label="Confirm Password"
+                aria-describedby="basic-addon1"
+                onChange={handleChange}
+            />
+          </InputGroup>
+
+          </Container> 
+          <Button className="sign-up-button" onClick={signUpClicked} variant="secondary" size="sm">Sign Up</Button>
         </>
     )
 }
